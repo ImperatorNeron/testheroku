05175edc80e6916849da893cb39d378ac8d8bfee
ImperatorNeron <katebarspk@gmail.com>
2024-04-27 20:17:44 +0300

    12

--- backproject/urls.py
+++ backproject/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from backproject.settings import DEBUG
-from backproject import settings
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls"), name="main"),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-if DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
