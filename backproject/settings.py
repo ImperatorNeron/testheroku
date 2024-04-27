5ebd1e18ff96185f660f07155c5b252d1eebecde
ImperatorNeron <katebarspk@gmail.com>
2024-04-27 21:03:14 +0300

    17

--- backproject/settings.py
+++ backproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-&f)p+7m^ih%m2!jr534bxfa3hwrfm6m7*ut$_(4)kzyc9%clg+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["backproject.herokuapp.com"]
+ALLOWED_HOSTS = ["backproject.herokuapp.com", "127.0.0.1"]
 
 # Application definition
 
